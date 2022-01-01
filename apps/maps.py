632c91b7ba4240255c0749a0848785832fbe7ffc
Kenneth Liao <32213208+kenneth-liao@users.noreply.github.com>
2022-01-01 15:04:16 -0800

    added chart and map apps. testing navigation.

---
+++ apps/maps.py
@@ -0,0 +1,19 @@
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+
+import pandas as pd
+import pathlib
+
+from app import app
+
+# get relative data folder
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("../data").resolve()
+
+data = pd.read_csv(DATA_PATH.joinpath("owid-covid-data.csv"))
+
+
+layout = html.Div([
+    html.H1('MAPS TEST')
+])
--- index.py
+++ index.py
@@ -11,16 +11,36 @@ from dash.dependencies import Input, Output
 from app import app
 
 # connect app pages
-from apps import page1
+from apps import chart
+from apps import maps
 
 app.layout = html.Div([
     html.H1('Hello World'),
+    # the Location component stores the url in the address bar
+    dcc.Location(id='url', refresh=False),
     html.Div([
         html.H5('Metric'),
         dcc.Dropdown(id='metric',
-                     options=[{'label':'Confirmed cases', 'value':'cases'}])
-    ], style={'width': '200px'})
+                     options=[{'label': 'Confirmed cases', 'value': 'cases'}])
+    ], style={'width': '200px'}),
+    # the page-content Div is where we will display the plots
+    html.Div(id='page-content', children=[]),
+    html.Div([
+        dcc.Link(' Chart |', href='/apps/chart'),
+        dcc.Link(' Map |', href='/apps/map')
+    ])
 ])
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def update_content(pathname):
+    if pathname == '/apps/chart':
+        return chart.layout
+    elif pathname == '/apps/map':
+        return maps.layout
+    else:
+        return '404 Error. Please click a link.'
 
 
 if __name__ == '__main__':
