1f1dfc74851bdac4adc0fc81a139c0aa51575b87
Kenneth Liao <32213208+kenneth-liao@users.noreply.github.com>
2022-01-01 17:26:33 -0800

    broke out the app layout into individual sections.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import dash
+import dash_bootstrap_components as dbc
 
 # instantiate app
 # these meta tags configure the app to be viewable on mobile devices
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial_scale=1.0'}])
+                            'content': 'width=device-width, initial_scale=1.0'}],
+                external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # instantiate server
 server = app.server
--- index.py
+++ index.py
@@ -13,42 +13,110 @@ from dash.dependencies import Input, Output
 # connect app.py file
 from app import app
 
-# connect app pages
-from apps import chart
-from apps import maps
-
+import pandas as pd
+from plotly import graph_objects as go
 import plotly.io as pio
 
 # set the default theme for plotly
 pio.templates.default = "presentation"
 
+# style definitions for sidebar and main content
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+    "overflow": "scroll"
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+    "display": "inline-block"
+}
+
+# load data
+data = pd.read_csv('data/owid-covid-data.csv')
+
+sidebar = html.Div(
+    dcc.Checklist(id='location', value="United States",
+                  options=[{'label': c, 'value': c} for c in data.location.unique()],
+                  persistence=True, persistence_type='memory'), style=SIDEBAR_STYLE
+)
+
+plotting_options = dbc.Row([
+    dbc.Col(
+        html.Div([
+            html.H6('Metric'),
+            dcc.Dropdown(id='metric', value='Confirmed cases', clearable=False,
+                         options=[{'label': 'Confirmed cases', 'value': 'cases'}],
+                         persistence=True, persistence_type='memory')
+        ], style={'width': 250})
+    ),
+    dbc.Col(
+        html.Div([
+            html.H6('Interval'),
+            dcc.Dropdown(id='interval', value='Cumulative', clearable=False,
+                         options=[{'label': 'Cumulative', 'value': 'total'}],
+                         persistence=True, persistence_type='memory')
+        ], style={'width': 250})
+    ),
+    dbc.Col(
+        html.Div([
+            dcc.Checklist(id='relative', value='Cumulative',
+                          options=[{'label': ' Relative to Population', 'value': 'relative'}],
+                          persistence=True, persistence_type='memory')
+        ]), align='end'
+    ),
+    dbc.Col(
+
+    ),
+])
+
 app.layout = html.Div([
-    html.H1('Hello World'),
+    html.H1('COVID-19 Global Data Tracking'),
     # the Location component stores the url in the address bar
     dcc.Location(id='url', refresh=False),
-    html.Div([
-        html.H5('Metric'),
-        dcc.Dropdown(id='metric', value='Confirmed cases', clearable=False,
-                     options=[{'label': 'Confirmed cases', 'value': 'cases'}])
-    ], style={'width': '200px'}),
-    # the page-content Div is where we will display the plots
-    html.Div(id='page-content', children=[]),
-    html.Div([
-        dcc.Link(' Chart |', href='/apps/chart'),
-        dcc.Link(' Map |', href='/apps/map')
+
+
+    dbc.Row([
+        dbc.Col(sidebar, width=2),
+
+        dbc.Col([
+            plotting_options,
+
+            # graph object which will display the data view
+            html.Div(dcc.Graph(id='visualization', figure={})),
+
+            # bottom navigation menu
+            html.Div([
+
+            ])
+        ])
     ])
 ])
 
 
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def update_content(pathname):
-    if pathname == '/apps/chart':
-        return chart.layout
-    elif pathname == '/apps/map':
-        return maps.layout
-    else:
-        return '404 Error. Please click a link.'
+@app.callback(Output('visualization', 'figure'),
+              [Input('location', 'value'),
+               Input('metric', 'value')])
+def update_figure(location, metric):
+    traces = []
+    for country in location:
+        traces.append(
+            go.Scatter(name=country,
+                       x=data[data['location'] == country]['date'],
+                       y=data['total_cases'])
+        )
+
+    fig = go.Figure(data=traces)
+    fig.update_layout(showlegend=True)
+
+    return fig
 
 
 if __name__ == '__main__':
