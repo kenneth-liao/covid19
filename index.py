39b39856399d1480f830460f6fda46a01287e524
Kenneth Liao <32213208+kenneth-liao@users.noreply.github.com>
2022-01-03 12:42:08 -0800

    Updated layout

--- index.py
+++ index.py
@@ -20,119 +20,97 @@ import plotly.io as pio
 # set the default theme for plotly
 pio.templates.default = "presentation"
 
-# style definitions for sidebar and main content
-SIDEBAR_STYLE = {
-    #"position": "fixed",
-    #"top": 70,
-    #"left": 0,
-    "bottom": 70,
-    "width": "25rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-    "overflow": "scroll"
-}
 
-CONTENT_STYLE = {
-    #"position": "fixed",
-    "top": 70,
-    "left": 300,
-    "bottom": 70,
-    "margin-left": "2rem",
-    "padding": "2rem 1rem"
-}
+# USER DEFINED STYLE SETTINGS #
+app_width = 8
+##################
+margin = (12-app_width)/2
+
 
 # load data
 data = pd.read_csv('data/owid-covid-data.csv')
 
+
 # define the various cards that will be displayed in the app
-title = dbc.Card(
-    dbc.CardBody(
-        html.H1('COVID-19 Global Data Tracking')
-    ), color='#4F33FF'
-)
-
-location_sidebar = dbc.Card(
-    dbc.CardBody(
-        dcc.Checklist(id='location',
-                      value=['United States', 'United Kingdom', 'Germany', 'Canada', 'Italy'],
-                      options=[{'label': c, 'value': c} for c in data.location.unique()]
-                      )
-    ), style={'width': '20rem'}, color='#FF7A33'
-)
-
-plotting_options = dbc.CardGroup([
-    dbc.Card(
-            dbc.CardBody([
-                html.H6('Metric'),
-                dcc.Dropdown(id='metric', value='Confirmed cases', clearable=False,
-                             options=[{'label': 'Confirmed cases', 'value': 'cases'}])
-            ])
-
-    ),
-    dbc.Card(
-            dbc.CardBody([
-                html.H6('Interval'),
-                dcc.Dropdown(id='interval', value='Cumulative', clearable=False,
-                             options=[{'label': 'Cumulative', 'value': 'total'}])
-            ])
-    ),
-    dbc.Card(
-            dbc.CardBody(
-                dcc.Checklist(id='relative', value='Cumulative',
-                              options=[{'label': ' Relative to Population', 'value': 'relative'}])
-            )
+def location_checklist():
+    return dcc.Checklist(
+        id='location', value=['United States', 'United Kingdom', 'Germany', 'Canada', 'Italy'],
+        options=[{'label': c, 'value': c} for c in data.location.unique()]
     )
-], style={'width': '70rem'})
 
 
-
-
-
-graph = dbc.Card(
-    dbc.CardBody(
-        html.Div(dcc.Graph(id='visualization', figure={}))
-    ), color='#3396FF'
-)
-
+def visualization():
+    return dcc.Graph(
+        id='visualization', figure={}
+    )
 
 
 app.layout = html.Div([
-    # # the Location component stores the url in the address bar
-    # dcc.Location(id='url', refresh=False),
-    #
-    #
-    # dbc.Row([
-    #     dbc.Col(sidebar, width=2),
-    #
-    #     dbc.Col(
-    #         html.Div([
-    #             plotting_options,
-    #
-    #             # graph object which will display the data view
-    #             dbc.Row(
-    #                 dbc.Col(
-    #                     html.Div(dcc.Graph(id='visualization', figure={})),
-    #                 )
-    #             ),
-    #
-    #             # bottom navigation menu
-    #             dbc.Row(
-    #                 dbc.Col(
-    #                     html.Div([
-    #
-    #                     ])
-    #                 )
-    #             )
-    #         ], style=CONTENT_STYLE)
-    #     )
-    # ])
-    dbc.Row(title),
-    dbc.Row([
-        dbc.Col(location_sidebar, width='auto'),
-        dbc.Col([
-            dbc.Row(plotting_options),
-            dbc.Row(graph)], width=8)
+    # the Location component stores the url in the address bar
+    dcc.Location(id='url', refresh=False),
+
+    dbc.Row(  # title
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody(
+                    html.H1('COVID-19 Global Data Dashboard')
+                ), className='mb-4', style={'background-color': '#FF6E33'}
+            ), width={'size': app_width, 'offset': margin}
+        )
+    ),
+
+    dbc.Row([  # logo & plotting options
+        dbc.Col(  # logo
+            dbc.Card(
+                dbc.CardBody(
+                    html.H1('Logo')
+                ), className='mb-2'
+            ), width={'size': 2, 'offset': margin}
+        ),
+        dbc.Col([  # plotting options
+            dbc.CardGroup([
+                dbc.Card(
+                    dbc.CardBody(
+                        html.H4('Metric')
+                    )
+                ),
+                dbc.Card(
+                    dbc.CardBody(
+                        html.H4('Interval')
+                    )
+                ),
+                dbc.Card(
+                    dbc.CardBody(
+                        html.H4('Relative to Population')
+                    )
+                ),
+                dbc.Card(
+                    dbc.CardBody(
+                        # empty placeholder
+                    )
+                )
+            ], className='mb-2', style={'background-color': '#FF6E33'})
+        ], width={'size': 10-2*margin})
+    ]),
+
+    dbc.Row([  # country check list & visualization
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody(
+                    # dcc checklist is rendered inside a div so it can be styled
+                    location_checklist(), style={'height': '60vh', 'overflow': 'auto'}
+                ), className='mb-2'
+            ), width={'size': 2, 'offset': margin}
+        ),
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody(
+                    visualization(), style={'height': '60vh'}
+                ), className='mb-2'
+            ), width={'size': 10-2*margin}
+        )
     ])
+
 ])
 
 
