f0df82fc258db963629f300f7de19b3cf392292f
Kenneth Liao <32213208+kenneth-liao@users.noreply.github.com>
2022-01-05 16:21:49 -0800

    Changed dbc theme to MATERIA

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import dash_bootstrap_components as dbc
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial_scale=1.0'}],
-                external_stylesheets=[dbc.themes.SPACELAB]
+                external_stylesheets=[dbc.themes.MATERIA]
                 )
 
 # instantiate server
